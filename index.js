cfb34daeea18d6fccb8482771a6780527245e54e
Marc Mauri Alloza <marcmaurialloza@gmail.com>
2024-05-14 17:55:48 +0200

    add update page and navigation between lists

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', async (req, res) => {
     try {
         const resp = await axios.get(instruments, { headers });
         const data = resp.data.results;
-        res.render('instruments', { title: 'Instruments | HubSpot APIs', data });
+        res.render('homepage', { title: 'Instruments | HubSpot APIs', data });
     } catch (error) {
         console.error(error);
     }
@@ -32,6 +32,9 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integration With HubSpot I Practicum' });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
